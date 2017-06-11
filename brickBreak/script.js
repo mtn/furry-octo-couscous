a955f2b1b2abc0a56bff9b195f5e3a34083ae070
Michael Noronha <michaeltnoronha@gmail.com>
2017-06-10 23:06:06 -0600

    Some progress on basic mechanics, but not much

--- brickBreak/script.js
+++ brickBreak/script.js
@@ -4,9 +4,17 @@ var canvasContext;
 var ballX = 75, ballY = 75;
 var ballSpeedX = 2, ballSpeedY = 2;
 
+const PADDLE_WIDTH = 200, PADDLE_HEIGHT = 20, PADDLE_OFFSET = 5;
+var paddleX;
+
 window.onload = function(){
     canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
+
+    canvas.addEventListener('mousemove',function(evt){
+        var mousePos = getMousePos(evt);
+        paddleX = mousePos.x - (PADDLE_WIDTH/2);
+    });
 
     var framesPerSecond = 30;
     setInterval(function(){
@@ -27,11 +35,12 @@ function move(){
 
 function render(){
     drawRectangle('black',0,0,canvas.width,canvas.height);
-
-    canvasContext.fillStyle = 'white';
-    canvasContext.beginPath();
-    canvasContext.arc(ballX,ballY,10,0,Math.PI*2,true);
-    canvasContext.fill();
+    drawRectangle('white',
+        paddleX,
+        canvas.height-PADDLE_HEIGHT-PADDLE_OFFSET,
+        PADDLE_WIDTH,
+        PADDLE_HEIGHT);
+    drawCircle('white',ballX,ballY,10);
 }
 
 function drawRectangle(color,posX,posY,width,height){
@@ -39,4 +48,22 @@ function drawRectangle(color,posX,posY,width,height){
     canvasContext.fillRect(posX,posY,width,height);
 }
 
+function drawCircle(color,posX,posY,radius){
+    canvasContext.fillStyle = color;
+    canvasContext.beginPath();
+    canvasContext.arc(posX,posY,radius,0,Math.PI*2,true);
+    canvasContext.fill();
+}
+
+function getMousePos(evt){
+    var rect = canvas.getBoundingClientRect(), root = document.documentElement;
+
+    var mouseX = evt.clientX - rect.left - root.scrollLeft;
+    var mouseY = evt.clientY - rect.top - root.scrollTop;
+
+    return {
+        x : mouseX,
+        y: mouseY
+    };
+}
 
